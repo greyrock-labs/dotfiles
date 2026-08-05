ee2d4a2d29c5f702ba891599731887204f2c8c33
Todd Punderson <tpunderson@greyrock.io>
2026-08-05 09:48:48 -0400

    refactor(fish): match upstream 00-env.fish layout

--- home/dot_config/fish/conf.d/00-env.fish
+++ home/dot_config/fish/conf.d/00-env.fish
@@ -1,7 +1,3 @@
-if test (uname) = Darwin
-    set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-end
-
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 
@@ -10,5 +6,7 @@ set -gx LANG "en_US.utf-8"
 set -gx VISUAL vim
 set -gx EDITOR vim
 set -gx KUBE_EDITOR vim
+
+set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 
 fish_add_path --global --prepend $HOME/.local/bin
